f87c3d3808d965c5b90d26062985dc72838e1a9c
jlhde123 ASUS <472327024@qq.com>
2018-01-23 12:01:36 +0800

    增加不同类型数据库处理

--- src/main/java/db/mysql/model/DataBaseTypeEnum.java
+++ src/main/java/db/mysql/model/DataBaseTypeEnum.java
@@ -1,5 +1,8 @@
 package db.mysql.model;
 
+import db.mysql.process.metadata.DataBaseMetaDataProcess;
+import db.mysql.process.metadata.MysqlMetaDataProcess;
+
 /**
  * db.mysql.model
  *
@@ -7,26 +10,50 @@ package db.mysql.model;
  * @date 2018/1/21 20:25
  */
 public enum DataBaseTypeEnum {
-    Mysql("mysql","com.mysql.jdbc.Driver"),
-    Oracle("oracle","oracle.jdbc.driver.OracleDriver"),
-    Postgres("postgres","org.postgresql.Driver")
+    Mysql("mysql","com.mysql.jdbc.Driver",new  MysqlMetaDataProcess()),
+    Oracle("oracle","oracle.jdbc.driver.OracleDriver",null),
+    Postgres("postgres","org.postgresql.Driver",null)
     ;
 
 
-    DataBaseTypeEnum(String dataBaseTypeName,String driver) {
+    DataBaseTypeEnum(String dataBaseTypeName, String driver, DataBaseMetaDataProcess metaDataProcess) {
         this.dataBaseTypeName = dataBaseTypeName;
         this.driver = driver;
+        this.metaDataProcess = metaDataProcess;
     }
 
     private String dataBaseTypeName;
     private String driver;
+    private DataBaseMetaDataProcess metaDataProcess;
 
+    public DataBaseMetaDataProcess getMetaDataProcess() {
+        return metaDataProcess;
+    }
 
+    public void setMetaDataProcess(DataBaseMetaDataProcess metaDataProcess) {
+        this.metaDataProcess = metaDataProcess;
+    }
 
-    public static String getDriver(String dataBaseTypeName){
+    public String getDataBaseTypeName() {
+        return dataBaseTypeName;
+    }
+
+    public void setDataBaseTypeName(String dataBaseTypeName) {
+        this.dataBaseTypeName = dataBaseTypeName;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public static DataBaseTypeEnum getByName(String dataBaseTypeName){
         for (DataBaseTypeEnum dataBaseTypeEnum :DataBaseTypeEnum.values()){
             if (dataBaseTypeEnum.dataBaseTypeName.equals(dataBaseTypeName)){
-                return dataBaseTypeEnum.driver;
+                return dataBaseTypeEnum;
             }
         }
         throw new RuntimeException("illegal database type");
