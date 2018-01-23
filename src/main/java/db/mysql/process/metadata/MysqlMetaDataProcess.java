9612cdde0539e3bfadc24e024819660b987b84fa
jlhde123 ASUS <472327024@qq.com>
2018-01-23 12:09:06 +0800

    增加不同类型数据库处理

--- src/main/java/db/mysql/process/metadata/MysqlMetaDataProcess.java
+++ src/main/java/db/mysql/process/metadata/MysqlMetaDataProcess.java
@@ -89,44 +89,6 @@ public class MysqlMetaDataProcess implements DataBaseMetaDataProcess{
                     mySqlData.setIsKey(0);
                 }
                 mySqlDataList.add(mySqlData);
-//                String tableCat = rs.getString("TABLE_CAT");//表目录（可能为空）
-//                String tableSchemaName = rs.getString("TABLE_SCHEM");//表的架构（可能为空）
-//                String tableName_ = rs.getString("TABLE_NAME");//表名
-//                String columnName = rs.getString("COLUMN_NAME");//列名
-//                int dataType = rs.getInt("DATA_TYPE"); //对应的java.sql.Types类型
-//                String dataTypeName = rs.getString("TYPE_NAME");//java.sql.Types类型   名称
-//                int columnSize = rs.getInt("COLUMN_SIZE");//列大小
-//                int decimalDigits = rs.getInt("DECIMAL_DIGITS");//小数位数
-//                int numPrecRadix = rs.getInt("NUM_PREC_RADIX");//基数（通常是10或2）
-//                int nullAble = rs.getInt("NULLABLE");//是否允许为null
-//                String remarks = rs.getString("REMARKS");//列描述
-//                String columnDef = rs.getString("COLUMN_DEF");//默认值
-//                int sqlDataType = rs.getInt("SQL_DATA_TYPE");//sql数据类型
-//                int sqlDatetimeSub = rs.getInt("SQL_DATETIME_SUB");   //SQL日期时间分?
-//                int charOctetLength = rs.getInt("CHAR_OCTET_LENGTH");   //char类型的列中的最大字节数
-//                int ordinalPosition = rs.getInt("ORDINAL_POSITION");  //表中列的索引（从1开始）
-//
-//                /**
-//                 * ISO规则用来确定某一列的为空性。
-//                 * 是---如果该参数可以包括空值;
-//                 * 无---如果参数不能包含空值
-//                 * 空字符串---如果参数为空性是未知的
-//                 */
-//                String isNullAble = rs.getString("IS_NULLABLE");
-//
-//                /**
-//                 * 指示此列是否是自动递增
-//                 * 是---如果该列是自动递增
-//                 * 无---如果不是自动递增列
-//                 * 空字串---如果不能确定它是否
-//                 * 列是自动递增的参数是未知
-//                 */
-//                String isAutoincrement = rs.getString("IS_AUTOINCREMENT");
-//
-//                System.out.println(tableCat + "-" + tableSchemaName + "-" + tableName_ + "-" + columnName + "-"
-//                        + dataType + "-" + dataTypeName + "-" + columnSize + "-" + decimalDigits + "-" + numPrecRadix
-//                        + "-" + nullAble + "-" + remarks + "-" + columnDef + "-" + sqlDataType + "-" + sqlDatetimeSub
-//                        + charOctetLength + "-" + ordinalPosition + "-" + isNullAble + "-" + isAutoincrement + "-");
             }
         } catch (SQLException e){
             e.printStackTrace();
